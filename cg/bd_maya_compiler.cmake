a6957e0cb17d5f82999ad630a5c404230e7c64b5
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2018-04-19 14:20:29 +0300

    fix: separate compiler setup for vray for maya

---
+++ cg/bd_maya_compiler.cmake
@@ -0,0 +1,38 @@
+#
+# Build system based on CMake
+#
+# Copyright (c) iCube R&D Group, www.rendering.ru
+#
+# These coded instructions, statements, and computer programs contain
+# unpublished proprietary information written by iCube R&D Group.
+# They may not be disclosed to third parties or copied or duplicated in
+# any form, in whole or in part, without the prior written consent of
+# iCube R&D Group.
+#
+set(MAYA_VERSION "2016" CACHE STRING "Maya version")
+
+if(MAYA_VERSION STREQUAL 2009)
+	set(MSVC_COMPILER 2005)
+elseif(MAYA_VERSION STREQUAL 2010)
+	set(MSVC_COMPILER 2008)
+elseif(MAYA_VERSION STREQUAL 2011)
+	set(MSVC_COMPILER 2008)
+elseif(MAYA_VERSION STREQUAL 2012)
+	set(MSVC_COMPILER 2008)
+elseif(MAYA_VERSION STREQUAL 2013)
+	set(MSVC_COMPILER 2010)
+elseif(MAYA_VERSION STREQUAL 2013.5)
+	set(MSVC_COMPILER 2010)
+elseif(MAYA_VERSION STREQUAL 2014)
+	set(MSVC_COMPILER 2010)
+elseif(MAYA_VERSION STREQUAL 2015)
+	set(MSVC_COMPILER 2012)
+elseif(MAYA_VERSION STREQUAL 2016)
+	set(MSVC_COMPILER 2012)
+elseif(MAYA_VERSION STREQUAL 2016.5)
+	set(MSVC_COMPILER 2012)
+elseif(MAYA_VERSION STREQUAL 2017)
+	set(MSVC_COMPILER 2012)
+elseif(MAYA_VERSION STREQUAL 2018)
+	set(MSVC_COMPILER 2015)
+endif()
---
+++ cg/bd_vray_for_maya_compiler.cmake
@@ -0,0 +1,15 @@
+#
+# Build system based on CMake
+#
+# Copyright (c) iCube R&D Group, www.rendering.ru
+#
+# These coded instructions, statements, and computer programs contain
+# unpublished proprietary information written by iCube R&D Group.
+# They may not be disclosed to third parties or copied or duplicated in
+# any form, in whole or in part, without the prior written consent of
+# iCube R&D Group.
+#
+
+if(VRAY_VERSION VERSION_GREATER_EQUAL 40)
+	set(MSVC_COMPILER 2017)
+endif()
