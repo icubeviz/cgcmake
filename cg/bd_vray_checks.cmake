e63a6ec332587898547cf660d981dcea419262e8
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2019-08-19 01:24:49 +0300

    fix: vraybase_ver.h support

--- cg/bd_vray_checks.cmake
+++ cg/bd_vray_checks.cmake
@@ -1,16 +1,29 @@
 function(bd_vray_detect_vray_version _vray_inc_paths _vray_lib_paths)
     if(WITH_CUSTOM_VRAY OR WITH_INSTALLED_VRAY)
-        find_file(VRAYBASE_H "vraybase.h"
+        find_file(VRAYBASE_H
+            NAMES
+                vraybase_ver.h vraybase.h
             PATHS
                 ${_vray_inc_paths}
             NO_DEFAULT_PATH
         )
+
         if(VRAYBASE_H)
+            set(VRAY_VERSION "40" CACHE STRING "" FORCE)
+
             file(STRINGS ${VRAYBASE_H} VRAY_DLL_VERSION REGEX "^#define[\t ]+VRAY_DLL_VERSION[\t ]+.*")
             string(REGEX REPLACE "^.*VRAY_DLL_VERSION[\t ]+([x0-9]*).*$" "\\1" VRAY_DLL_VERSION "${VRAY_DLL_VERSION}")
 
             if(NOT VRAY_DLL_VERSION)
-                set(VRAY_VERSION "40" CACHE STRING "" FORCE)
+                file(STRINGS ${VRAYBASE_H} VRAY_VERSION_MAJOR REGEX "^#define[\t ]+VRAY_DLL_VERSION_MAJOR[\t ]+.*")
+                string(REGEX REPLACE "^.*VRAY_DLL_VERSION_MAJOR[\t ]+([0-9]*).*$" "\\1" VRAY_VERSION_MAJOR "${VRAY_VERSION_MAJOR}")
+
+                file(STRINGS ${VRAYBASE_H} VRAY_VERSION_MINOR REGEX "^#define[\t ]+VRAY_DLL_VERSION_MINOR[\t ]+.*")
+                string(REGEX REPLACE "^.*VRAY_DLL_VERSION_MINOR[\t ]+([0-9]*).*$" "\\1" VRAY_VERSION_MINOR "${VRAY_VERSION_MINOR}")
+
+                message(STATUS "Found V-Ray SDK: ${VRAY_VERSION_MAJOR}.${VRAY_VERSION_MINOR}")
+
+                set(VRAY_VERSION "${VRAY_VERSION_MAJOR}${VRAY_VERSION_MINOR}" CACHE STRING "" FORCE)
             else()
                 string(SUBSTRING "${VRAY_DLL_VERSION}" 2 1 VRAY_VERSION_MAJOR)
                 string(SUBSTRING "${VRAY_DLL_VERSION}" 3 1 VRAY_VERSION_MINOR)
