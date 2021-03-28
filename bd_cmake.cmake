49b5ead6ded89b91dc970f3e7277f199f18ba77c
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2021-03-28 15:20:06 +0300

    add: product uuid; bd_parse_version()

--- bd_cmake.cmake
+++ bd_cmake.cmake
@@ -22,6 +22,7 @@ set(SDK_ROOT "${ICUBE_SDK}" CACHE PATH "SDK location" FORCE)
 set(RDGROUP_ROOT "${ICUBE_OUTPUT}")
 set(RDGROUP_RELEASE_ROOT    "${RDGROUP_ROOT}/Release"    CACHE PATH "Release location")
 set(RDGROUP_INSTALLERS_ROOT "${RDGROUP_ROOT}/Installers" CACHE PATH "Installers location")
+set(ICUBE_NAMESPACE "E934A4E8-2D3A-4A3C-8B55-93272EC9CE2A")
 
 set(DEVEL_ROOT    "$ENV{HOME}/install")
 set(LOCAL_INSTALL "$ENV{HOME}/install/rdgroup")
@@ -30,3 +31,13 @@ get_filename_component(BD_CMAKE ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
 set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH};${BD_CMAKE}/cg;${BD_CMAKE}/compiler;${BD_CMAKE}/find")
 
 set(CMAKE_VS_INCLUDE_INSTALL_TO_DEFAULT_BUILD ON)
+
+macro(bd_parse_version)
+    file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/version.h" VERSION_MAJOR REGEX "^#define[\t ]+VERSION_MAJOR[\t ]+.*")
+    string(REGEX REPLACE "^.*VERSION_MAJOR[\t ]+([0-9]*).*$" "\\1" VERSION_MAJOR "${VERSION_MAJOR}")
+
+    file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/version.h" VERSION_MINOR REGEX "^#define[\t ]+VERSION_MINOR[\t ]+.*")
+    string(REGEX REPLACE "^.*VERSION_MINOR[\t ]+([0-9]*).*$" "\\1" VERSION_MINOR "${VERSION_MINOR}")
+
+    message(STATUS "${PROJECT_NAME} Version: ${VERSION_MAJOR}.${VERSION_MINOR}")
+endmacro()
